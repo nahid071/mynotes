76475283c21d6e720ca9fd31c06c271b35838c0d
nahid071 <mdnahid071@gmail.com>
2022-09-13 03:23:04 +0600

    user regrister and login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express")
-const { registerUser } = require("../controllers/userController")
+const { protect } = require("../middleware/authMiddleware")
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/userController")
 
 const router = express.Router()
 
 router.post("/register", registerUser)
+router.post("/login", loginUser)
+router.get("/me", protect, getMe)
 
 module.exports = router
