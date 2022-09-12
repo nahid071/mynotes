68eb729646f4a6231cef572a4fbd45e86dafa1e0
nahid071 <mdnahid071@gmail.com>
2022-09-12 23:40:23 +0600

    server setup completed

---
+++ backend/controllers/userController.js
@@ -0,0 +1,7 @@
+const registerUser = (req, res) => {
+  res.send("User registered successfully!")
+}
+
+module.exports = {
+  registerUser,
+}
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+// const path = require("path")
+// const colors = require("colors")
+// const dotenv = require("dotenv").config()
+const { errorHandler } = require("./middleware/errorMiddleware")
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req, res) => {
+  res.send("API is running ...")
+})
+
+app.use("/api/users", require("./routes/userRoutes"))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
