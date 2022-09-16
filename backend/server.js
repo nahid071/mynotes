3bfaca6d975193517b1bf1b5ee14e98e0b06aacd
nahid071 <mdnahid071@gmail.com>
2022-09-16 15:07:49 +0600

    authentication register

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,37 @@
-const express = require("express")
+const express = require("express");
 // const path = require("path")
-const colors = require("colors")
-const dotenv = require("dotenv").config()
-const connectDB = require("./config/db")
-const { errorHandler } = require("./middleware/errorMiddleware")
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("API is running ...")
-})
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/notes", require("./routes/noteRoutes"));
 
-app.use("/api/users", require("./routes/userRoutes"))
-app.use("/api/notes", require("./routes/noteRoutes"))
+// serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.use(errorHandler)
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Dashboard from "./pages/Dashboard"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Header from "./layout/Header"
-import Profile from "./pages/Profile"
-import CreateNote from "./pages/CreateNote"
-import "./App.css"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./layout/Header";
+import Profile from "./pages/Profile";
+import CreateNote from "./pages/CreateNote";
+
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
   return (
@@ -27,8 +30,9 @@ function App() {
           </Routes>
         </div>
       </BrowserRouter>
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
